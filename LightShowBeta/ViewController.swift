4d32ee8f4dcebe0e0a1789fc4730cf33ca254d4c
Santiago Gutierrez <ssgutierrez42@gmail.com>
2019-05-25 20:55:11 -0700

    Initial Commit

---
+++ LightShowBeta/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LightShowBeta
+//
+//  Created by Santiago Gutierrez on 5/25/19.
+//  Copyright © 2019 zent. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
