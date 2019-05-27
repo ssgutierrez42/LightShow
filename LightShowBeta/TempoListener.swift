6a107c31fa76ec2a8332315c134f4b7cd9b87b5f
Santiago Gutierrez <ssgutierrez42@gmail.com>
2019-05-26 21:20:30 -0700

    first commit

---
+++ LightShowBeta/TempoListener.swift
@@ -0,0 +1,15 @@
+//
+//  TempoListener.swift
+//  LightShowBeta
+//
+//  Created by Santiago Gutierrez on 5/25/19.
+//  Copyright © 2019 zent. All rights reserved.
+//
+
+import AudioKit
+
+class TempoListener : AKMIDITempoListener {
+    
+    
+    
+}
--- LightShowBeta/ViewController.swift
+++ LightShowBeta/ViewController.swift
@@ -1,20 +1,51 @@
 //
 //  ViewController.swift
-//  LightShowBeta
+//  LightShow
 //
-//  Created by Santiago Gutierrez on 5/25/19.
+//  Created by Santiago Gutierrez on 5/22/19.
 //  Copyright © 2019 zent. All rights reserved.
 //
 
 import UIKit
+import CoreMIDI
+import AudioKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, AKMIDIListener, AKMIDITransformer {
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let midi = AudioKit.midi
+        print(midi.inputNames)
+        
+        midi.openInput()
+        midi.addListener(self)
+        
+        let tempo = TempoListener()
+        tempo.addObserver(self)
+        midi.addListener(tempo)
     }
-
-
+    
+    /*func transform(eventList: [AKMIDIEvent]) -> [AKMIDIEvent] {
+        var newList = [AKMIDIEvent]();
+        eventList.forEach { (event) in
+            var mutableEvent = AKMIDIEvent(data: [0xF8])
+            if let data = mutableEvent.data.first, data == 0xF8 {
+                //mutableEvent.command = .clock
+                
+                print("clock...")
+            }
+            newList.append(event)
+        }
+        //print(eventList)
+        return newList//[AKMIDIEvent(data: [0xF8])]
+    }*/
+    
 }
 
+extension ViewController: AKMIDITempoObserver {
+    
+    func receivedTempo(bpm: BPMType, label: String) {
+        print("bpm: \(bpm) and label: \(label)")
+    }
+    
+}
